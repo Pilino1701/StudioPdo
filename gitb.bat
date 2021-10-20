6917b6f295ba916fbd66c7cc63d542bbaef3c27f
Pilino1701 <pietrosara@yahoo.com.br>
2021-10-20 15:45:30 -0300

    Studio pdo n.2

--- gitb.bat
+++ gitb.bat
@@ -1,6 +1,6 @@
 git init
 git add --all
-git commit -m "Studio pdo n.1"
+git commit -m "Studio pdo n.2"
 git branch -M main
 git remote add origin https://github.com/Pilino1701/StudioPdo.git
 git push -u origin main
